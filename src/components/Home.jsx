4a72db9cd9afd40a36f51210b9d8aaba2562c9cf
josefrancis1 <127965648+josefrancis1@users.noreply.github.com>
2024-09-07 09:41:16 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,7 @@ import demo3 from '../assets/images/save2.jpg';
 import demo4 from '../assets/images/save3.jpg';
 import demo5 from '../assets/images/save4.jpg';
 
-const images = [demo2, demo3, demo4, demo5]; // Array of images
-
+const images = [demo2, demo3, demo4, demo5]; 
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
